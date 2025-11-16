c68cdf4f4782eeb56f6031834f822464b671cf06
HammadEngr <hammadahmed.engr@gmail.com>
2025-11-16 15:05:32 +0100

    BLOG urls registered

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def starting_page(request):
+    return HttpResponse("Index page")
+
+def posts(request):
+    return HttpResponse("POSTS")
+
+def post_detail(request, slug):
+    return HttpResponse("POST DETAIL")
