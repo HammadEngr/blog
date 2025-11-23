9387dd5b279f0ffa16610a6b5701305c55c6ebe0
HammadEngr <hammadahmed.engr@gmail.com>
2025-11-23 14:42:06 +0100

    UI styling and routes done

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,93 @@
+from datetime import date
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
+all_posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image":"mountains.jpg",
+        "author":"Awan",
+        "date": date(2025,7,21),
+        "title":"Mountains Hiking",
+        "excerpt":"There is nothing like the views you get when hiking in the mountains",
+        "content":'''
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    Quae quas laboriosam harum obcaecati provident aperiam rem commodi 
+                    asperiores dolorum officiis. 
+                    Aperiam quibusdam labore ipsam? 
+                    Iusto commodi placeat hic voluptatum maiores!
+                    
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    Quae quas laboriosam harum obcaecati provident aperiam rem commodi 
+                    asperiores dolorum officiis. 
+                    Aperiam quibusdam labore ipsam? 
+                    Iusto commodi placeat hic voluptatum maiores!
+                    
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    Quae quas laboriosam harum obcaecati provident aperiam rem commodi 
+                    asperiores dolorum officiis. 
+                    Aperiam quibusdam labore ipsam? 
+                    Iusto commodi placeat hic voluptatum maiores!
+                    '''
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.jpg",
+        "author": "Maximilian",
+        "date": date(2022, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpg",
+        "author": "Maximilian",
+        "date": date(2020, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+]
+
+def get_date(post):
+    return post["date"]
+
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+
+    return render(request, "blog/index.html", {"posts":latest_posts })
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {"all_posts":all_posts})
 
 def post_detail(request, slug):
-    return HttpResponse("POST DETAIL")
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {"post":identified_post})
